13983d5d1ff22eea00f5c43f8cb5ce24cc537eb7
Cameron <xwhalex@icloud.com>
2020-01-03 00:04:33 +0800

    Changes

---
+++ src/Test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Test = ({ title }) =>
+    <div>{title}</div>;
+export default Test;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from 'react-dom'
+import Test from './Test';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//Created first function component and passed props to it
+//Minimal React Setup with Babel && Webpack
+
+const title = "React with Webpack and Babel"
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+
+                <h1>Hello</h1>
+                <p> This is so fun</p>
+                <p>I like this</p>
+            </div>
+
+        )
+    }
+}
+export default App;
+
+
+
+ReactDOM.render(<Test title={title} />, document.getElementById("root"));
+module.hot.accept();
